cb6f6536af583e9c6ae8709ebeaa67b90e1f9351
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2021-10-04 17:11:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,43 +7,42 @@ import glob
 import time
 from option import *
 from utils import *
+from torch.autograd import Variable
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-# os.environ['CUDA_VISIBLE_DEVICES']='0' # For GPU only
+os.environ['CUDA_VISIBLE_DEVICES']='0' # For GPU only
 device = get_device()
 
+class Tester(): # TODO: replace image_path
+	def __init__(self):
+		self.scale_factor = 12
+		self.net = model.enhance_net_nopool(self.scale_factor, conv_type='dsc').to(device)
+		self.net.load_state_dict(torch.load(args.weight_dir, map_location=device))
 
-def Tester(image_path, args, scale_factor=12):
+	def inference(self, image_path):
+		# Read image from path
+		data_lowlight = image_from_path(image_path)
 
-	# Read image from path
-	data_lowlight = image_from_path(image_path)
+		# Scale image to have the resolution of multiple of 4
+		data_lowlight = scale_image(data_lowlight, self.scale_factor, device) if self.scale_factor != 1 else data_lowlight
 
-	# Scale image to have the resolution of multiple of 4
-	data_lowlight = scale_image(data_lowlight, scale_factor, device) if scale_factor != 1 else data_lowlight
+		# Run model inference
+		start = time.time()
+		enhanced_image, params_maps = self.net(data_lowlight)
+		end_time = (time.time() - start)
+		print(end_time)
 
-	# Build model
-	EFE_net = model.enhance_net_nopool(scale_factor, conv_type='dsc').to(device) # TAKE care of this conv
+		# Load result directory and save image
+		image_path = image_path.replace('test_data', args.test_dir)
+		result_path = image_path
+		if not os.path.exists(image_path.replace('/' + image_path.split("/")[-1], '')):
+			os.makedirs(image_path.replace('/' + image_path.split("/")[-1], ''))
+		torchvision.utils.save_image(enhanced_image, result_path)
 
-	# Load weight directory
-	EFE_net.load_state_dict(torch.load(args.weight_dir, map_location=device))
+		return end_time
 
-	# Run model inference
-	start = time.time()
-	enhanced_image,params_maps = EFE_net(data_lowlight)
-	end_time = (time.time() - start)
-	print(end_time)
-
-	# Load result directory and save image
-	image_path = image_path.replace('test_data',args.test_dir)
-	result_path = image_path
-	if not os.path.exists(image_path.replace('/'+image_path.split("/")[-1],'')):
-		os.makedirs(image_path.replace('/'+image_path.split("/")[-1],''))
-	torchvision.utils.save_image(enhanced_image, result_path)
-
-	return end_time
-
-def test():
-	with torch.no_grad():
+	def test(self):
+		self.net.eval()
 		filePath = args.input_dir
 		print(filePath)
 		file_list = os.listdir(filePath)
@@ -55,14 +54,18 @@ def test():
 
 		for image in test_list:
 			print(image)
-			sum_time = sum_time + Tester(image, args)
+			sum_time = sum_time + self.inference(image)
 
 		print(sum_time)
-		
+
+
+
 
 
 if __name__ == '__main__':
-	test()
+	t = Tester()
+	t.test()
+
 
 
 		
