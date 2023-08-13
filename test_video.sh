984b8640b0ba5ea546c5bd58f9f1b5f2f03dfef9
ShenZheng2000 <69662345+ShenZheng2000@users.noreply.github.com>
2023-08-12 22:42:39 -0400

    Update instruction for test_video.sh

--- test_video.sh
+++ test_video.sh
@@ -1,8 +1,8 @@
 # Video -> Image
 python demo/make_video.py --choice V2I
 
-# Image Inference
-python test.py
+# Image Inference (NOTE: specifiy these paths on your own)
+python test.py --input_dir $image_lowlight_folder --test_dir $image_folder
 
 # Image -> Video
 python demo/make_video.py --choice I2V
